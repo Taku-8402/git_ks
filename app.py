3c7947c36b1cdbbfd12192cd450870293d7fd95e
kamada tomoki <t.kamada.sys20@morijyobi.ac.jp>
2021-03-30 11:11:07 +0900

    鎌田作業終了

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.py("/")
+@app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/circle")
+def circle():
+    return render_template("circle.html")
+
+@app.route("/circle_ans")
+def circle_ans():
+    s = request.args.get("s")
+    a = int(s)**2*3.14
+    return render_template("circle_ans.html",a=a)
+    
 if __name__ == "__main__":
     app.run(debug=True)
